f4d695c1470080f7674d2e559e9bbea98714371b
aditya988 <adityav@gmail.com>
2015-01-26 03:52:40 +0530

    Pasted code into the function
    As part of Programming assignment 2, I have pasted the code into the R
    script file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this program is to cache the inverse of a matrix. It is assumed
+## that the matrix that is passed is a square invertible matrix.
 
-## Write a short comment describing this function
+## The following function is responsible setting the values of the inverse value
+## and storing it for retrieval.
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function is responsible for checking if the inverse has been cached or 
+## needs to be calculated anew. Based on that, the value of the inverse is 
+## either retrieved or calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("Getting cached data...")
+                return(inv)        
 }
